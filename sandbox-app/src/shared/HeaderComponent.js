538e8a1b0c8aa94e78abd97c8da7a87b3cd98951
bumdum <jared.support@gmail.com>
2018-12-23 18:13:39 -0700

    prettier, headercomp and footercomp

---
+++ sandbox-app/src/shared/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <header className="">
+                <nav>
+                    <ul />
+                </nav>
+            </header>
+        );
+    }
+}
+
+export default HeaderComponent;
